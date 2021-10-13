5ba42d9ed4814530664adc681e2a6e5d00af0501
vapene <parkjjong@hotmail.com>
2021-10-13 10:44:43 +0900

    warnings 2

--- logging_config_example.py
+++ logging_config_example.py
@@ -13,8 +13,7 @@ logger.info("난 할 수 있다")
 
 logger.warning('곧 문제 생김')
 logger.warning('곧 문제 생김')
-logger.warning('곧 문제 생김')
-logger.warning('곧 문제 생김')
+
 
 logger.error('기능이 동d작 안함')
 logger.critical('시스템 다운!')
