ceb4b1123585587402168ba1fd80ae03902d1a2c
vapene <parkjjong@hotmail.com>
2021-10-13 10:53:48 +0900

    warnings 2

--- logging_config_example.py
+++ logging_config_example.py
@@ -1,21 +1,21 @@
-import logging
-import logging.config
-
-logging.config.fileConfig('logging.conf')
-
-logger=logging.getLogger(__name__)
-
-
-logger.debug("yes i can")
-
-
-logger.info("난 할 수 있다")
-
-logger.warning('곧 문제 생김')
-logger.warning('곧 문제 생김')
-
-
-logger.error('기능이 동작 안함')
-logger.critical('시스템 다운!')
-
+# import logging
+# import logging.config
+#
+# logging.config.fileConfig('logging.conf')
+#
+# logger=logging.getLogger(__name__)
+#
+#
+# logger.debug("yes i can")
+#
+#
+# logger.info("난 할 수 있다")
+#
+# logger.warning('곧 문제 생김')
+# logger.warning('곧 문제 생김')
+#
+#
+# logger.error('기능이 동작 안함')
+# logger.critical('시스템 다운!')
+#
 
